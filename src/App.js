e866c41cb5b91b9c28a314cc4cabefac91fcce5e
Alexey Konstantinov <alexey.const@icloud.com>
2023-11-17 16:39:03 +0400

    add architect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header";
+import {Route, Routes} from 'react-router-dom'
+import MainPage from "./pages/MainPage";
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route index element={<MainPage />}/>
+      </Routes>
     </div>
   );
 }
