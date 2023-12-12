c22863a8be801118b0af3d7f670603f0302ffb15
Alexey Konstantinov <alexey.const@icloud.com>
2023-12-13 01:31:31 +0400

    add MainPage.js

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,7 +3,6 @@ import Header from "../components/Header";
 import ControlButtons from "../components/ControlButtons";
 import CardsSlider from "../components/CardsSlider";
 
-
 const MainPage = () => {
 
   return (
