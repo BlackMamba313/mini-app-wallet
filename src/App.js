092f761c2d17744c58273e91cf4aaf44ee0892d8
Alexey Konstantinov <alexey.const@icloud.com>
2024-02-05 17:32:55 +0400

    add MainPage.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import SellPage from "./pages/SellPage";
 import SendPage from "./pages/SendPage";
 import CreatePage from "./pages/CreatePage";
 import HistoryPage from "./pages/HistoryPage";
-// import { ApolloClient, InMemoryCache } from '@apollo/client';
-//
-// const client = new ApolloClient({
-//   uri: 'https://likepaywallet-e9c7714eec1c.herokuapp.com/graphql', // Замените на актуальный URL вашего сервера GraphQL
-//   cache: new InMemoryCache(),
-// });
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_SERVER_URI, // Замените на актуальный URL вашего сервера GraphQL
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const {tg} = useTelegram();
+  console.log(client)
   useEffect(() => {
-
     tg.ready();
   }, [tg])
 
