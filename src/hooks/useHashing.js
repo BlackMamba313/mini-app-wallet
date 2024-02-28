644f105b25ab656ef4df3a10fc59b2d366c04462
Alexey Konstantinov <alexey.const@icloud.com>
2024-02-28 21:07:42 +0400

    add MainPage.js

--- src/hooks/useHashing.js
+++ src/hooks/useHashing.js
@@ -2,48 +2,39 @@ import CryptoJS from 'crypto-js';
 
 function useHashing() {
   // Функция для создания HMAC SHA-256 хеша
-  const hmacSHA256Hash = (dataString, secretKey) => {
-    // Преобразование секретного ключа из строки в WordArray, если необходимо
-    const hash = CryptoJS.HmacSHA256(dataString,"|", secretKey);
-    console.log('hashing:', dataString, secretKey);
+  const hmacSHA256Hash = (dataString, secretKeyHex) => {
+    // Преобразование шестнадцатеричного представления секретного ключа в WordArray для CryptoJS
+    const secretKey = CryptoJS.enc.Hex.parse(secretKeyHex);
+    const hash = CryptoJS.HmacSHA256(dataString, secretKey);
     return hash.toString(CryptoJS.enc.Hex);
   };
 
   // Функция для создания отсортированной строки из объекта данных
   const createSortedDataString = (dataToHash) => {
-    // Преобразование логических значений в 1
-    Object.keys(dataToHash).forEach(key => {
-      if (typeof dataToHash[key] === 'boolean') {
-        dataToHash[key] = dataToHash[key] ? 1 : 0;
+    // Преобразование и исключение несущественных значений
+    const filteredDataToHash = Object.entries(dataToHash).reduce((acc, [key, value]) => {
+      if (value || typeof value === 'number' || value === 0) {
+        acc[key] = typeof value === 'boolean' ? 1 : value;
       }
-    });
+      return acc;
+    }, {});
 
-    // Добавляем временную метку
-    dataToHash.ti = Math.floor(Date.now() / 1000);
+    filteredDataToHash.ti = Math.floor(Date.now() / 1000); // Добавляем временную метку
 
-    // Сортируем ключи и создаем строку в формате 'ключ=значение', разделенную переносами строк
-    const sortedKeys = Object.keys(dataToHash).sort();
-    const dataString = sortedKeys.map(key => `${key}=${dataToHash[key]}`).join('\n');
-    console.log('Sorted data string for hashing:', dataString);
-
-    return dataString;
+    const sortedKeys = Object.keys(filteredDataToHash).sort();
+    return sortedKeys.map(key => `${key}=${filteredDataToHash[key]}`).join('\n');
   };
 
   // Функция хеширования данных
   const hash = (dataToHash) => {
-    // Создаем отсортированную строку из данных
     const dataString = createSortedDataString(dataToHash);
+    const secretKeyHex = process.env.REACT_APP_SECRET_KEY_HASH; // Получаем ключ в виде шестнадцатеричной строки
 
-    // Используем секретный ключ из переменных окружения
-    const secretKey = process.env.REACT_APP_SECRET_KEY_HASH;
+    const si = hmacSHA256Hash(dataString, secretKeyHex); // Генерируем подпись
 
-    // Создаем HMAC SHA-256 хеш отсортированной строки и секретного ключа
-    const si = hmacSHA256Hash(dataString, secretKey);
-
-    // Возвращаем хеш и исходные данные с добавленной подписью
     return {
       hashedData: si,
-      requestData: { ...dataToHash, si }
+      requestData: { ...dataToHash, si: si }
     };
   };
 
@@ -51,6 +42,7 @@ function useHashing() {
 }
 
 export default useHashing;
+
 
 
 
