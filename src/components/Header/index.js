3cd31019259b2807def5fccb8dbb95836c4b3dcb
Alexey Konstantinov <alexey.const@icloud.com>
2024-03-04 01:01:21 +0400

    change auth

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Header.module.css';
 import ReturnIcon from "../../assets/ReturnIcon";
 import {useNavigate} from "react-router-dom";
@@ -6,11 +6,14 @@ import {useSelector} from "react-redux";
 import {currentRate, userFiat} from "../../store/currency/selectors";
 
 const Header = ({title, activeWallet}) => {
+  const [update, setUpdate] = useState(true);
   const navigate = useNavigate();
   const fiat = useSelector(userFiat);
   const rate = useSelector(currentRate)
-
-
+  useEffect(() => {
+    setUpdate( !update); // Принудительное обновление компонента при изменении activeWallet
+  }, [activeWallet]);
+  console.log(update)
   return (
     <div>
       {title && <div className={styles.header}>
