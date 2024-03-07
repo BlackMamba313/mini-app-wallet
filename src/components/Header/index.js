4b908e243b005a0fa367509b3d5567c9666afe88
Alexey Konstantinov <alexey.const@icloud.com>
2024-03-07 12:04:45 +0400

    change auth

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,14 +3,13 @@ import styles from './Header.module.css';
 import ReturnIcon from "../../assets/ReturnIcon";
 import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
-import {currentRate, userFiat} from "../../store/currency/selectors";
+import {userFiat} from "../../store/currency/selectors";
 import {walletData} from "../../store/auth/selectors";
 
 const Header = ({title}) => {
   const activeWallet  = useSelector(walletData)
   const navigate = useNavigate();
   const fiat = useSelector(userFiat);
-  const rate = useSelector(currentRate)
   return (
     <div>
       {title && <div className={styles.header}>
@@ -21,7 +20,7 @@ const Header = ({title}) => {
       </div>
       }
     <div className={styles.cost}>
-      По курсу за 1 {activeWallet.token} {rate} {fiat}
+      По курсу за 1 {activeWallet.token} ??? {fiat}
     </div>
     </div>
   );
