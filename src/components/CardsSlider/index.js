46163ab3d468a14123bdeeac0b6666b5a4f8b8d0
Alexey Konstantinov <alexey.const@icloud.com>
2024-03-11 00:46:26 +0400

    change auth

--- src/components/CardsSlider/index.js
+++ src/components/CardsSlider/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import SwipeableViews from 'react-swipeable-views';
+import { bindKeyboard } from 'react-swipeable-views-utils';
 import Card from "../Card";
-import {useDispatch, useSelector} from "react-redux";
-import {walletsData} from "../../store/auth/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { walletsData } from "../../store/auth/selectors";
 import { setActiveWallet } from "../../store/currency";
+
+const EnhancedSwipeableViews = bindKeyboard(SwipeableViews);
 
 const CardsSlider = () => {
   const dispatch = useDispatch();
-  const wallets  = useSelector(walletsData);
+  const wallets = useSelector(walletsData);
   const [currentIndex, setCurrentIndex] = React.useState(0);
+
   const handleChangeIndex = (index) => {
     setCurrentIndex(index);
-    dispatch(setActiveWallet(wallets[index]))
+    dispatch(setActiveWallet(wallets[index]));
   };
-  return (
-    <SwipeableViews index={currentIndex} onChangeIndex={handleChangeIndex}>
 
-      {wallets && wallets.map((card) => (
-        <Card key={card.token} network={card.network} address={card.address} token={card.token} balance={card.balance}/>
+  return (
+    <EnhancedSwipeableViews index={currentIndex} onChangeIndex={handleChangeIndex}>
+      {wallets && wallets.map((card, index) => (
+        <Card key={index} network={card.network} address={card.address} token={card.token} balance={card.balance} />
       ))}
-    </SwipeableViews>
+    </EnhancedSwipeableViews>
   );
 };
 
