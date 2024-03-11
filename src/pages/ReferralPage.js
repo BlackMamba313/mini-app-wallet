6d37108f838a75890108629a793132deca6c68fc
Alexey Konstantinov <alexey.const@icloud.com>
2024-03-11 15:09:10 +0400

    change auth

--- src/pages/ReferralPage.js
+++ src/pages/ReferralPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import KPIShow from "../components/KPIShow";
+import Header from "../components/Header";
 
 const ReferralPage = () => {
   return (
     <>
+      <Header title='Партнерская программа'/>
       <KPIShow/>
     </>
   );
