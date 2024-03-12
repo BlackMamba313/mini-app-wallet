f884cd958b67332ff19b0e34bfb94db3dfff01f4
Alexey Konstantinov <alexey.const@icloud.com>
2024-03-12 17:51:07 +0400

    change auth

--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -3,22 +3,9 @@ import styles from './ShareButton.module.css';
 
 const ShareButton = ({ address }) => {
   const handleShare = async () => {
-    if (navigator.share) {
-      try {
-        await navigator.share({
-          title: 'Поделиться адресом',
-          text: `Мой адрес кошелька: ${address}`,
-          // url: document.location.href, // Можно опустить, если хотите поделиться только текстом
-        });
-        console.log('Содержимое успешно поделено.');
-      } catch (error) {
-        console.error('Произошла ошибка при попытке поделиться: ', error);
-      }
-    } else {
       // Создаем ссылку для отправки сообщения в Telegram
       const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(document.location.href)}&text=${encodeURIComponent(`Мой адрес кошелька: ${address}`)}`;
       window.open(telegramUrl, '_blank');
-    }
   };
 
   return (
