14f9f41da4f470ee7d9047ed3ca2be0ad7047714
Alexey Konstantinov <alexey.const@icloud.com>
2024-03-19 19:21:14 +0400

    change auth

--- src/components/KPIShow/index.js
+++ src/components/KPIShow/index.js
@@ -8,17 +8,17 @@ import useHashing from "../../hooks/useHashing";
 
 const KPIShow = () => {
   const dispatch = useDispatch();
-  const { hash } = useHashing();
+  const {hash} = useHashing();
   const {id} = useSelector(userData)
   const stat = useSelector(refStatData)
   // Функция для обработки нажатия кнопки поделиться
   const shareProject = () => {
-    const refShare =`${process.env.REACT_APP_URL_JOKER_REG}?ref=${id}`
+    const refShare = `${process.env.REACT_APP_URL_JOKER_REG}?ref=${id}`
     navigator.clipboard.writeText(refShare);
   };
 
   useEffect(() => {
-    const { requestData } = hash({id:id});
+    const {requestData} = hash({id: id});
     dispatch(GetStat(requestData))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -26,21 +26,24 @@ const KPIShow = () => {
   console.log('GetStat>>>>', stat)
   return (
     <div className={styles.container}>
-      <div className={styles.stat}>
-        Total users<br />
-        <span className={styles.statValue}>{stat.count}</span>
-      </div>
-      <div className={styles.stat}>
-        Total balance<br />
-        <span className={styles.statValue}>{stat.bal} usdt</span>
-      </div>
-      <CircularProgressBar value={stat.kpi}/>
-      <div
-        className={styles.shareButton}
-        onClick={shareProject}
-      >
-        Share Project
-      </div>
+      {stat && <>
+        <div className={styles.stat}>
+          Total users<br/>
+          <span className={styles.statValue}>{stat.count}</span>
+        </div>
+        <div className={styles.stat}>
+          Total balance<br/>
+          <span className={styles.statValue}>{stat.bal} usdt</span>
+        </div>
+        <CircularProgressBar value={stat.kpi}/>
+        <div
+          className={styles.shareButton}
+          onClick={shareProject}
+        >
+          Share Project
+        </div>
+      </>
+      }
     </div>
   );
 };
